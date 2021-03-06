50d3c98231a3f5160d37317005624dc5e124027c
congchu <cookie00421@gmail.com>
2021-03-06 19:54:46 +0900

    상세 페이지: 문제 풀기 기본 기능 완료

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,14 @@ import { convertPlainText } from "./ManagerSentence";
 import { IPracticeBundle } from "./interface/IPracticeBundle";
 
 function App() {
-  const [tryAnswer, setTryAnswer] = useState("");
-  const [visibleTryAnswer, setVisibleTryAnswer] = useState(false);
+  const [textInWrinting, setTextInWrinting] = useState("");
+  const [tryText, setTryText] = useState("");
+  const [visibletryText, setVisibletryText] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false);
+  const [visibleAnswer, setVisibleAnswer] = useState(false);
   const [visibleIsCorrect, setVisibleIsCorrect] = useState(false);
   const [targetPractice, setTargetPractice] = useState<IPracticeBundle>();
+  const [nextIndex, setNextIndex] = useState(0);
 
   useEffect(() => {
     const parsed = queryString.parse(window.location.search);
@@ -24,63 +27,183 @@ function App() {
     const targetPractice = practiceBundle[targetIndex];
 
     setTargetPractice(targetPractice);
+    const nextIndex = (targetIndex + 1) % practiceBundle.length;
+    setNextIndex(nextIndex);
   }, []);
 
-  const compareAnswer = (targetPractice: IPracticeBundle) => {
+  const compareAnswer = (
+    targetPractice: IPracticeBundle,
+    textInWrinting: string
+  ) => {
     const correctPlainText = convertPlainText(targetPractice.enTexts[0]);
-    const tryPlainText = convertPlainText(tryAnswer);
+    const tryPlainText = convertPlainText(textInWrinting);
     if (correctPlainText === tryPlainText) {
       setIsCorrect(true);
     } else {
       setIsCorrect(false);
     }
+    setTryText(textInWrinting);
 
-    setVisibleTryAnswer(true);
+    setVisibletryText(true);
     setVisibleIsCorrect(true);
+  };
+
+  // 유저가 '정답보기' 버튼을 누른 경우
+  const showAnswer = (targetPractice: IPracticeBundle) => {
+    setVisibleIsCorrect(false);
+    setVisibleAnswer(true);
+    setTryText(targetPractice.enTexts[0]);
   };
 
   if (targetPractice) {
     return (
-      <div className="App">
-        <header className="header">
-          <h1 className="">Koo's 영작 연습소 </h1>
-        </header>
-        <section>
-          <div className="flex">
-            <div>한글: </div> <div>{targetPractice.korText}</div>
-          </div>
+      <>
+        <nav>
+          <div>영작 연습소</div>
+        </nav>
+        <div className="App">
+          <header className="flex">
+            <article className="flex-center">
+              {/* 이미지  */}
+              {targetPractice.related_images ? (
+                <div>
+                  <img
+                    width="300"
+                    src={targetPractice.related_images[0].link}
+                  />
+                </div>
+              ) : null}
+            </article>
+            <article className="right-article flex-center">
+              {/* 상황 설명 */}
+              <div>
+                {targetPractice.situation ? (
+                  <div
+                    style={{
+                      color: "gray",
+                      fontSize: "14px",
+                      whiteSpace: "pre",
+                      padding: "10px",
+                    }}
+                  >
+                    {targetPractice.situation}
+                  </div>
+                ) : null}
+              </div>
+              <div>{targetPractice.korText}</div>
 
-          <div className="flex">
-            <div> 영어: </div>
-            <input
-              name="english_sentence"
-              id="english_input"
-              onChange={(e) => setTryAnswer(e.target.value)}
-            />
-          </div>
-          <div className="flex">
-            <button
-              className="btn-white"
-              onClick={() => compareAnswer(targetPractice)}
-            >
-              맞추기
-            </button>
-            <button
-              onClick={() => setVisibleTryAnswer(!visibleTryAnswer)}
-              className="btn-white"
-            >
-              답보기
-            </button>
-          </div>
-          {visibleTryAnswer ? <div>{tryAnswer}</div> : null}
-
-          {visibleIsCorrect ? (
-            <div>
-              {isCorrect ? <>맞았습니다!! 👏</> : <div>틀렸어요 😹</div>}
-            </div>
+              <div>
+                <input
+                  width="500"
+                  name="english_sentence"
+                  id="english_input"
+                  onChange={(e) => setTextInWrinting(e.target.value)}
+                />
+              </div>
+              <div>
+                {isCorrect || visibleAnswer ? (
+                  <button
+                    onClick={() =>
+                      (window.location.href = `?index=${nextIndex}`)
+                    }
+                  >
+                    다음 문제
+                  </button>
+                ) : (
+                  <>
+                    <button
+                      className="btn-white"
+                      onClick={() =>
+                        compareAnswer(targetPractice, textInWrinting)
+                      }
+                    >
+                      {tryText ? "다시 도전!" : "정답 도전!"}
+                    </button>
+                    {tryText ? (
+                      <button
+                        className="btn-white"
+                        onClick={() => showAnswer(targetPractice)}
+                      >
+                        정답보기
+                      </button>
+                    ) : null}
+                  </>
+                )}
+              </div>
+              {visibletryText ? <div>{tryText}</div> : null}
+              {visibleIsCorrect ? (
+                <>
+                  <div>
+                    {isCorrect ? <>맞았습니다!! 👏</> : <div>틀렸어요 😹</div>}
+                  </div>
+                  <div></div>
+                </>
+              ) : null}
+            </article>
+          </header>
+          {isCorrect || visibleAnswer ? (
+            <>
+              <section className="flex">
+                <article>
+                  {/* 또 다른 표현 */}
+                  {targetPractice.enTexts.length > 1 ? (
+                    <div>
+                      <h3>또 다른 표현</h3>
+                      {targetPractice.enTexts.map((item, index) => {
+                        if (index > 0) {
+                          return <div>{item}</div>;
+                        }
+                      })}
+                    </div>
+                  ) : null}
+                </article>
+                <article>
+                  {/* 문제 해설 */}
+                  {targetPractice.helpGrammars ? (
+                    <div>
+                      {targetPractice.helpGrammars.map((item) => (
+                        <>
+                          <h3>{item.title}</h3>
+                          <div style={{ whiteSpace: "pre-line" }}>
+                            {item.description}
+                          </div>
+                        </>
+                      ))}
+                    </div>
+                  ) : null}
+                </article>
+              </section>
+            </>
           ) : null}
-        </section>
-      </div>
+
+          {isCorrect || visibleAnswer ? (
+            <section>
+              {/* 영상 해설*/}
+              <article>
+                {targetPractice.helpVideos ? (
+                  <div>
+                    {targetPractice.helpVideos.map((item) => (
+                      <>
+                        <h3>{item.title}</h3>
+                        <div style={{ whiteSpace: "pre-line" }}>
+                          <iframe
+                            width="300"
+                            height="180"
+                            src={item.link}
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                          ></iframe>
+                        </div>
+                      </>
+                    ))}
+                  </div>
+                ) : null}
+              </article>
+            </section>
+          ) : null}
+        </div>
+      </>
     );
   } else {
     return (
