afd5da9446cd8317b0d1bfcd8ad894cd1998f5f9
congchu <cookie00421@gmail.com>
2021-03-06 12:35:04 +0900

    PracticeBundle interface 오타 수정

--- src/interface/IPracticeBundle.tsx
+++ src/interface/IPracticeBundle.tsx
@@ -1,24 +1,18 @@
 export interface IPracticeBundle {
-  related_images?: [
-    {
-      link: string;
-      desc?: string;
-    }
-  ];
+  related_images?: {
+    link: string;
+    desc?: string;
+  }[];
   situation?: string; // 상황이 있으면.
   korText: string; //한글 문장.
   korTextDesc?: string; // 한글문장 부연 설명.
   enTexts: string[]; // 영어 정답. 첫번째가 대표 문장.
-  helpGrammars?: [
-    {
-      title: string; // 문법 제목
-      description: string; // 문법 설명
-    }
-  ];
-  helpVideos?: [
-    {
-      title: string; //영상 자료 제목
-      link: string; // 영장 자료 링크
-    }
-  ];
+  helpGrammars?: {
+    title: string; // 문법 제목
+    description: string; // 문법 설명
+  }[];
+  helpVideos?: {
+    title: string; //영상 자료 제목
+    link: string; // 영장 자료 링크
+  }[];
 }
