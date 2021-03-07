61c0ca35faddeaacb09ef5e846e4302ddb4149b2
congchu <cookie00421@gmail.com>
2021-03-07 17:19:43 +0900

    practice interface에 date랑 type 컬럼 추가

--- src/interface/IPractice.tsx
+++ src/interface/IPractice.tsx
@@ -1,18 +1,20 @@
-export interface IPracticeBundle {
+export interface IPractice {
   related_images?: {
     link: string;
     desc?: string;
   }[];
+  date: string; // 날짜
+  type: string[]; // 자료 타입
   situation?: string; // 상황이 있으면.
   korText: string; //한글 문장.
   korTextDesc?: string; // 한글문장 부연 설명.
   enTexts: string[]; // 영어 정답. 첫번째가 대표 문장.
   helpGrammars?: {
-    title: string; // 문법 제목
-    description: string; // 문법 설명
+    title?: string; // 문법 제목
+    description?: string; // 문법 설명
   }[];
-  helpVideos?: {
-    title: string; //영상 자료 제목
-    link: string; // 영장 자료 링크
+  related_videos?: {
+    title?: string; //영상 자료 제목
+    link?: string; // 영장 자료 링크
   }[];
 }
