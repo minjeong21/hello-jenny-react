137ce6cfb4b5dc79d5984354a0a98a6589e6039b
congchu <cookie00421@gmail.com>
2021-04-09 14:23:36 +0900

    영작 문제 가져오는 API 리팩토링

--- src/apis/PracticeApi.tsx
+++ src/apis/PracticeApi.tsx
@@ -1,100 +1,37 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_AIRTABLE_PRACTICE_API,
-  // timeout: 2000,
-  headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-  },
+  baseURL: process.env.REACT_APP_MOCK_SERVER_API,
+  timeout: 5000,
+  // headers: {
+  //   Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+  // },
 });
 
 export const fetchPractices = async () => {
-  return instance
-    .get("", {
-      params: {
-        view: "Default",
-        maxRecords: 100,
-      },
-    })
-    .then(function (response) {
-      if (response && response.data && response.data.records) {
-        return response.data.records;
-      } else {
-        return null;
-      }
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-      return error;
-    });
+  return instance.get("/list/").catch(function (error) {
+    console.log(error);
+    return error;
+  });
 };
 
-export const fetchPracticeByNumId = async (numId: number) => {
-  return instance
-    .get("", {
-      params: {
-        view: "Default",
-        maxRecords: 1,
-        filterByFormula: `numid=${numId}`,
-      },
-    })
-    .then(function (response) {
-      if (response && response.data && response.data.records) {
-        return response.data.records;
-      } else {
-        return null;
-      }
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-      return error;
-    });
+export const fetchPracticeByNumId = async (id: number) => {
+  return instance.get(`/practice/${id}/`).catch(function (error) {
+    console.log(error);
+    return error;
+  });
 };
 
 export const fetchPracticeByTheme = async (theme: string) => {
-  return instance
-    .get("", {
-      params: {
-        view: "Default",
-        maxRecords: 100,
-        filterByFormula: `{themes}='${theme}'`,
-      },
-    })
-    .then(function (response) {
-      if (response && response.data && response.data.records) {
-        return response.data.records;
-      } else {
-        return null;
-      }
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-      return error;
-    });
+  return instance.get(`/pracitce/theme/${theme}/list/`).catch(function (error) {
+    console.log(error);
+    return error;
+  });
 };
 
 export const fetchPracticeByLevel = async (level: string) => {
-  return instance
-    .get("", {
-      params: {
-        view: "Default",
-        maxRecords: 100,
-        filterByFormula: `level=${level}`,
-      },
-    })
-    .then(function (response) {
-      if (response && response.data && response.data.records) {
-        return response.data.records;
-      } else {
-        return null;
-      }
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-      return error;
-    });
+  return instance.get(`/pracitce/level/${level}/list/`).catch(function (error) {
+    console.log(error);
+    return error;
+  });
 };
