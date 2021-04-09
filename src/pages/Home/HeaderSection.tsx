29dc5e237c6bc07fc46ccc1782819c4d73f52e90
congchu <cookie00421@gmail.com>
2021-04-09 18:17:56 +0900

    Home : 토끼 이미지, header 텍스트 변경, header 사진과 텍스트 위치 변경

--- src/pages/Home/HeaderSection.tsx
+++ src/pages/Home/HeaderSection.tsx
@@ -1,7 +1,8 @@
 import { Box, Image } from "grommet";
 const HeaderSection = ({ viewSize }: { viewSize: string }) => {
-  const mainText = `따끈따끈~ 오늘의 문장이 도착했어요!\n같이 한번 풀어볼까요?? `;
-  const subText = "문제를 모두 맞춘다면, 기분 좋은 하루가 될꺼에요!!";
+  const mainText = `Hello, I’m Jenny!\n나랑 친구가 되어줄래?`;
+  const subText =
+    "나랑 친구가 되면, 네가 영어로 글을 쓸 수 있게 만들어줄께 !! \n어려울것 같아? 걱정마!\n내 힌트는 너를 영어 천재로 만들어 줄거야! ";
   return (
     <Box background="#faf8f8">
       <header>
@@ -15,7 +16,7 @@ const HeaderSection = ({ viewSize }: { viewSize: string }) => {
               <div className="flex-1 text-center">
                 <Image src="/assets/header-rabit-bottom.png" width="200px" />
               </div>
-              <div className="flex-2 flex flex-column justify-center pad-m">
+              <div className="flex-2 flex flex-column justify-center pad-m ">
                 <div className="pre-line font-large font pb-xs">{mainText}</div>
                 <div className="font-small font-gray-3">{subText}</div>
               </div>
@@ -23,13 +24,19 @@ const HeaderSection = ({ viewSize }: { viewSize: string }) => {
             </>
           ) : (
             <>
-              <div className="flex-2 flex flex-column justify-center pad-m">
-                <div className="pre-line font-large font pb-xs">{mainText}</div>
-                <div className="font-small font-gray-3">{subText}</div>
-              </div>
               {/* 이미지 */}
               <div className="flex-1">
-                <Image src="/assets/header-rabit3.png" width="300px" />
+                <Image src="/assets/header-rabit.png" width="300px" />
+              </div>
+              <div className="flex-2 flex flex-column justify-center pad-m">
+                <div className="bg-lightpink radius-10 pad-l">
+                  <div className="pre-line font-large font pb-xs">
+                    {mainText}
+                  </div>
+                  <div className="font-small font-gray-3 pre-line">
+                    {subText}
+                  </div>
+                </div>
               </div>
             </>
           )}
