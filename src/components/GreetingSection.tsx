e3d7b8d5efe0ddbdb276582abff1ace01628bc29
congchu <cookie00421@gmail.com>
2021-05-01 18:22:28 +0900

    Fixed: Home Greeting 섹션 수정

--- src/components/GreetingSection.tsx
+++ src/components/GreetingSection.tsx
@@ -1,47 +1,29 @@
-const GreetingSection = ({ viewSize }: { viewSize: string }) => {
-  const mainText = `나랑 영어공부 할래? 내가 도와줄께!!`;
-  const subText =
-    "나는 힌트 왕이거든! \n내 힌트는 너를 영어 천재로 만들어 줄거야! ";
+import { HOME_GREETING_DESC, HOME_GREETING_HEADING } from "properties/Text";
+import useWindowSize from "utils/useWindowSize";
+
+const GreetingSection = () => {
+  const { width } = useWindowSize();
+
   return (
     <div>
-      <header>
-        <div
-          className={` pt-l margin-center ${
-            viewSize === "small" ? "flex-column" : "flex"
-          }`}
-        >
-          {viewSize === "small" ? (
-            <>
-              <div className="flex-1 text-center">
-                <img src="/assets/header-rabit-bottom.png" width="200px" />
-              </div>
-              <div className="flex-2 flex flex-column justify-center pad-m ">
-                <div className="pre-line font-large font pb-xs">{mainText}</div>
-                <div className="font-small font-gray-3">{subText}</div>
-              </div>
-              {/* 이미지 */}
-            </>
-          ) : (
-            <>
-              {/* 이미지 */}
-              <div className="flex-1">
-                <img src="/assets/header-rabit.png" width="300px" />
-              </div>
-              <div className="flex-2 flex flex-column justify-center pad-m">
-                <div className="bg-lightpink radius-10 pad-l">
-                  <div className="pre-line font-large font pb-xs">
-                    {mainText}
-                  </div>
-                  <div className="font-small font-gray-3 pre-line">
-                    {subText}
-                  </div>
-                </div>
-              </div>
-            </>
-          )}
-          {/* Text */}
+      <div
+        className={` pt-l margin-center ${width > 680 ? "flex" : "flex-col"}`}
+      >
+        <div className="flex-1">
+          <img src="/assets/header-rabit.png" width="300px" />
         </div>
-      </header>
+        <div className="flex-2 flex flex-column justify-center pad-m">
+          <div className="bg-lightpink radius-10 pad-l">
+            <div className="pre-line font-large font pb-xs">
+              {HOME_GREETING_HEADING}
+            </div>
+            <div className="font-small font-gray-3 pre-line">
+              {HOME_GREETING_DESC}
+            </div>
+          </div>
+        </div>
+        {/* 이미지 */}
+      </div>
     </div>
   );
 };
---
+++ src/properties/Text.tsx
@@ -0,0 +1,3 @@
+export const HOME_GREETING_HEADING = `나랑 영어공부 할래? 내가 도와줄께!!`;
+export const HOME_GREETING_DESC =
+  "나는 힌트 왕이거든! \n내 힌트는 너를 영어 천재로 만들어 줄거야! ";
