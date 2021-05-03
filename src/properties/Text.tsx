65695c1fb617029b64ca7fd90b232e672e23f2eb
congchu <cookie00421@gmail.com>
2021-05-03 10:15:48 +0900

    Fixed: Greeting 텍스트 업데이트

--- src/properties/Text.tsx
+++ src/properties/Text.tsx
@@ -1,3 +1,3 @@
-export const HOME_GREETING_HEADING = `안녕! 나는 제니야, \n나랑 영어 공부할래?`;
+export const HOME_GREETING_HEADING = `안녕하세오! 저는 제니! 3살이에요. \n저랑 영작놀이 하실래요?`;
 export const HOME_GREETING_DESC =
-  "\n어려울것 같아? 걱정마! \n힌트와 함께라면 게임처럼 즐거울꺼야! \n 네가 영어로 글을 쓸 수 있게 만들어줄께!  ";
+  "\n어려울것 같다고요? 걱정마세요. 힌트와 함께라면 게임처럼 즐거울꺼에요. \n 머리가 따끈따끈해지는 기분이 들꺼에요! ";
