e8f51492106a8c80e5bcb13aa2d19ee18780212e
congchu <cookie00421@gmail.com>
2021-05-05 00:55:57 +0900

    Fixed: 필터 메뉴 태그 메뉴로 변경

--- src/components/molecules/FilterNavigation.tsx
+++ src/components/molecules/FilterNavigation.tsx
@@ -7,21 +7,43 @@ const Navgation = styled.ul`
 `;
 const FilterNavigation = () => {
   return (
-    <Navgation className="flex">
-      <div className="w-full pb-3">
-        <div className="inline-block mr-2 mt-2">
-          <button className="focus:outline-none text-blue-600 text-sm py-2 px-4 rounded-md border border-blue-600 hover:bg-blue-50">
-            모든형식
+    <Navgation className="text-sm flex mb-2">
+      <div className="text-sm w-full pb-3">
+        <div className="text-sm inline-block mr-2 mt-2">
+          <button className="text-sm mr-2">난이도</button>
+          <button className="text-sm text-blue-600 text-sm p-1 mr-2 rounded-md hover:bg-blue-50 bg-blue-200 border">
+            #아주_쉬워요
+          </button>
+          <button className="text-sm text-blue-600 text-sm p-1 mr-2 rounded-md hover:bg-blue-50">
+            #조금_쉬워요
+          </button>
+          <button className="text-sm text-blue-600 text-sm p-1 mr-2 rounded-md hover:bg-blue-50">
+            #중간_난이도
+          </button>
+          <button className="text-sm text-blue-600 text-sm p-1 mr-2 rounded-md hover:bg-blue-50">
+            #약간_어려워요
+          </button>
+          <button className="text-sm text-blue-600 text-sm p-1 mr-2 rounded-md hover:bg-blue-50">
+            #매우_어려워요
+          </button>
+          <button className="text-sm text-blue-600 text-sm p-1 mr-2 rounded-md hover:bg-blue-50">
+            #초고수
           </button>
         </div>
-        <div className="inline-block mr-2 mt-2">
-          <button className="focus:outline-none text-gray-600 text-sm py-2 px-4 rounded-md border border-gray-600 hover:bg-gray-50">
-            제니모드
+        <br />
+        <div className="text-sm inline-block mr-2 mt-2">
+          <button className="text-sm mr-2">테마</button>
+          <button className="text-sm bg-green-100 text-green-600 text-sm p-1 mr-2 rounded-md border-green-600 hover:bg-green-50">
+            #친구_만들기
           </button>
-        </div>
-        <div className="inline-block mr-2 mt-2">
-          <button className="focus:outline-none text-green-600 text-sm py-2 px-4 rounded-md border border-green-600 hover:bg-green-50">
-            모든테마
+          <button className="text-sm text-green-600 text-sm p-1 mr-2 rounded-md border-green-600 hover:bg-green-50">
+            #비즈니스_영어
+          </button>
+          <button className="text-sm text-green-600 text-sm p-1 mr-2 rounded-md border-green-600 hover:bg-green-50">
+            #영화_명대사
+          </button>
+          <button className="text-sm text-green-600 text-sm p-1 mr-2 rounded-md border-green-600 hover:bg-green-50">
+            #노래_한곡_외우기
           </button>
         </div>
       </div>
