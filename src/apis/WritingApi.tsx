0a5689c6b323e08aa9adf2e8da5eb54ee42fdbfe
congchu <cookie00421@gmail.com>
2021-05-18 22:01:11 +0900

    Fix: API 호출 timeout 시간 변경

--- src/apis/WritingApi.tsx
+++ src/apis/WritingApi.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_DFR_API,
-  timeout: 5000,
+  timeout: 10000,
 });
 
 export const fetchRepWriting = async () => {
