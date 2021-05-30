ac9f08b4993ae78e131d0e5f273880cee9e28c8a
congchu <cookie00421@gmail.com>
2021-05-30 23:04:49 +0900

    Feat: jest 로직 추가

---
+++ src/test/writing.test.js
@@ -0,0 +1,35 @@
+import Writing from "utils/Writing";
+
+const writingObject = {
+  kr_sentence: "오늘은 두통이 심하다. ",
+  en_sentence: "I've a bad headache today. ",
+};
+const writing = new Writing(writingObject);
+
+test("앞 뒤 공백 제거", () => {
+  expect(writing.getAnswerSentence()).toBe("I've a bad headache today.");
+});
+
+test("50% 단어 맞춤", () => {
+  const UserTryText = "I've a ";
+  const precent = writing.getMatchedWordPercent(UserTryText);
+  expect(precent).toBe(40);
+});
+
+test("Get Plain Answer", () => {
+  const result = writing.getAnswerSentencePlain();
+  const userText = "I`ve been have. ";
+  const userResult = writing.convertSentencePlain(userText);
+
+  expect(result).toBe("I've a bad headache today");
+  expect(userResult).toBe("I've been have");
+});
+
+test("Trim", () => {
+  const UserTryText = "today";
+
+  const words = writing.getCompareUserSentenceWords(UserTryText);
+  console.log(words);
+
+  expect(words).toBe(words);
+});
